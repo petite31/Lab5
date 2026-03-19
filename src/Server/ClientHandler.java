799c375cfd09698c5d791a61467e82b153cebe05
Nguyen Ngoc Lan <ngoclannguyen31012005@gmail.com>
2026-03-19 22:41:13 +0700

    final đi ???

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -2,13 +2,13 @@ package Server;
 
 import java.net.*;
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ClientHandler extends Thread {
     private Socket socket;
 
-    public ClientHandler(Socket socket) { this.socket = socket; }
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
 
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -24,34 +24,37 @@ public class ClientHandler extends Thread {
                 case "REGISTER":
                     out.println(FileManage.register(data[1]));
                     break;
+
                 case "LOGIN":
                     out.println(FileManage.login(data[1]));
-                    break;
-                case "SEND":
-                    String toUser = data[1];
-                    String subject = data.length > 2 ? data[2] : "No_Subject";
-                    String content = data.length > 3 ? data[3] : "";
-
-                    // Log Server
-                    System.out.println("\n[SERVER LOG] Received an email sending request:");
-                    System.out.println(" ↳ Recipient account identified: " + toUser);
-                    System.out.println(" ↳ Email subject: " + subject);
-
-                    // Save
-                    String result = FileManage.saveEmail(toUser, subject, content);
-
-                    // Console
-                    if (result.startsWith("SUCCESS")) {
-                        System.out.println(" ↳ Successfully created file in directory: MailData/" + toUser);
-                    } else {
-                        System.out.println(" ↳ FAILED: " + result.split("\\|")[1]);
-                    }
                     break;
 
                 case "READ":
                     out.println(FileManage.readEmail(data[1], data[2]));
                     break;
+
+                case "SEND":
+                    String toUser = data[1];
+                    String subject = data.length > 2 ? data[2] : "No_Subject";
+                    String content = data.length > 3 ? data[3] : "";
+
+                    System.out.println("\n[SERVER LOG] Received an email sending request:");
+                    System.out.println(" ↳ Recipient: " + toUser);
+                    System.out.println(" ↳ Subject: " + subject);
+
+                    String result = FileManage.saveEmail(toUser, subject, content);
+
+                    if (result.startsWith("SUCCESS")) {
+                        System.out.println(" ↳ Successfully created file in directory: MailData/" + toUser);
+                    } else {
+                        System.out.println(" ↳ FAILED: " + result.split("\\|")[1]);
+                    }
+
+                    out.println(result);
+                    break;
             }
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
